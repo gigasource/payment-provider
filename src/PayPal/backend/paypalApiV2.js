eb9b53db9691669b04b1eb4dcdd9a3e465f69668
ThinhVu <vutrongthinhk7@gmail.com>
2020-06-05 17:03:50 +0700

    Add PayPal provider

---
+++ src/PayPal/backend/paypalApiV2.js
@@ -0,0 +1,163 @@
+;"use strict";
+const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
+const { TransactionsGetRequest } = require('./requests/transactionsGetRequest');
+
+async function captureOrder(payPalClient, orderId, debug=false) {
+  try {
+    const request = new checkoutNodeJssdk.orders.OrdersCaptureRequest(orderId);
+    request.requestBody({});
+    const response = await payPalClient.execute(request);
+    if (debug){
+      console.log("Status Code: " + response.statusCode);
+      console.log("Status: " + response.result.status);
+      console.log("Capture ID: " + response.result.id);
+      console.log("Links:");
+      response.result.links.forEach((item, index) => {
+        let rel = item.rel;
+        let href = item.href;
+        let method = item.method;
+        let message = `\t${rel}: ${href}\tCall Type: ${method}`;
+        console.log(message);
+      });
+      // To toggle print the whole body comment/uncomment the below line
+      console.log(JSON.stringify(response.result, null, 4));
+    }
+    return response;
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+/**
+ * Execute list transaction query
+ * @param payPalClient
+ * @param query
+ * @param debug
+ * @returns {Promise<*>}
+ * @private
+ */
+async function _execListTransactionRequest(payPalClient, query, debug) {
+  try {
+    const request = new TransactionsGetRequest(query);
+    const response = await payPalClient.execute(request);
+    if (debug)
+      console.log(response);
+    return response
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+/**
+ * Execute single/multiple transaction queries base on params
+ * @param store_id
+ * @param payPalClient
+ * @param start_date
+ * @param end_date
+ * @param output
+ * @param page_index
+ * @param page_size
+ * @returns {Promise<{lastRefreshedDateTime: *, transactions: []}|*[]>}
+ */
+async function getTransactions(payPalClient, {
+  /*Allow filter*/
+  start_date, end_date,
+  /*Allow options field to get transaction order detail*/
+  output,
+  /*Pagination*/
+  page_index,
+  page_size,
+  transaction_status,
+  transaction_type,
+  fields
+}) {
+  const query =  { start_date, end_date };
+
+  if (page_size)
+    query.page_size = page_size;
+
+  if (transaction_status)
+    query.transaction_status = transaction_status;
+
+  if (transaction_type)
+    query.transaction_type = transaction_type;
+
+  if (fields)
+    query.fields = fields;
+
+  const responses = [];
+  // exec first query
+  let response = await _execListTransactionRequest(payPalClient, query);
+  if (response && response.statusCode === 200) {
+    responses.push(response);
+
+    // continue execute to gather all transaction
+    // TODO: Known-issue: Load all transaction into memory for multiple request at the same time lead to memory issue
+    if (response.result.total_pages > 1) {
+      const queries = [];
+      for(let i=2; i<=response.result.total_pages; ++i)
+        queries.push({...query, page: i});
+      responses.push.apply(responses, await Promise.all(queries.map(qry => _execListTransactionRequest(payPalClient, qry))))
+    }
+
+    const transactions = [];
+    responses.forEach(({ statusCode, result }) => {
+      if (statusCode === 200 && result && Array.isArray(result.transaction_details) && result.transaction_details.length > 0)
+        transactions.push.apply(transactions, result.transaction_details)
+    });
+
+    return {
+      transactions,
+      totalPages: response.result.total_pages,
+      lastRefreshedDateTime: response.result.last_refreshed_datetime
+    }
+  } else {
+    return []
+  }
+}
+
+/**
+ * Get transaction detail
+ * @param payPalClient
+ * @param transaction_id
+ * @param start_date
+ * @param end_date
+ * @param output
+ * @returns {Promise<null|{transactions: *}>}
+ */
+async function getTransactionDetail(payPalClient, {
+  transaction_id,
+  start_date,
+  end_date,
+  transaction_status,
+  fields,
+}) {
+  const query =  {
+    transaction_id,
+    start_date,
+    end_date,
+  };
+
+  if (transaction_status)
+    query.transaction_status = transaction_status;
+
+  if (fields)
+    query.fields = fields;
+
+  // Note: A transaction ID is not unique in the reporting system.
+  // The response can list two transactions with the same ID.
+  // One transaction can be balance affecting while the other is non-balance affecting.
+  let { statusCode, result } = await _execListTransactionRequest(payPalClient, query);
+  if (statusCode === 200 && result) {
+      return { transactions: result.transaction_details }
+  } else {
+    return null
+  }
+}
+
+module.exports = {
+  captureOrder,
+  getTransactions,
+  getTransactionDetail,
+};
---
+++ src/PayPal/frontend/PayPalSmartButton.vue
@@ -0,0 +1,147 @@
+<template>
+  <div v-if="selfHost" :id="containerId"></div>
+</template>
+
+<script>
+  // https://developer.paypal.com/docs/checkout/reference/customize-sdk/#query-parameters
+  export default {
+    name: 'PayPalSmartButton',
+    props: {
+      // CONTAINER ------
+      selfHost: Boolean,
+      hostId: String,
+      
+      // order items
+      orderInfo: Object,
+      
+      // query parameters
+      clientId: String,
+      merchantId: String,
+      currency: String,
+      intent: String,
+      commit: Boolean,
+      vault: Boolean,
+      disableFunding: String,
+      disableCard: String,
+      integrationDate: String,
+      debug: Boolean,
+      buyerCountry: String,
+      locale: String,
+      
+      // behavior
+      /**
+       * If useCustomCreateOrder is set, onCreateOrder event will be emit
+       * else, PayPal built-in function actions.order.create will be call with provided orderInfo
+       *
+       * Use case: Set this value to true in-case you want to create order by server side code
+       */
+      useCustomCreateOrder: Boolean,
+
+      /**
+       * If useCustomCaptureOrder is set, onApprove event will be emit with data object contain transaction information
+       * else, current transaction will be captured
+       */
+      useCustomCaptureOrder: Boolean,
+    },
+    data: function () {
+      return {
+        sdkLoaded: false,
+        containerId: this.selfHost ? `ppsb-${new Date().getTime()}` : this.hostId,
+      }
+    },
+    mounted() {
+      const sdkHref = this.buildSdkHref();
+      let script = document.querySelector(`script[src="${sdkHref}"]`);
+      if (!script) {
+        this.debug && console.log('Loading PayPal sdk...');
+        script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = sdkHref;
+        script.onload = () => this.sdkLoaded = true;
+        document.body.appendChild(script)
+      } else {
+        this.sdkLoaded = true
+      }
+    },
+    watch: {
+      orderInfo() {
+        this.updatePayPalItemsDebounce()
+      },
+      sdkLoaded() {
+        if (this.sdkLoaded) {
+          this.updatePayPalItems()
+        }
+      }
+    },
+    created() {
+      this.updatePayPalItemsDebounce = _.debounce(this.updatePayPalItems, 200)
+    },
+    methods: {
+      buildSdkHref() {
+        const endpoint = 'https://www.paypal.com/sdk/js';
+        const qry = {};
+        if (!this.clientId) {
+          console.error('Missing client id');
+          return;
+        }
+        qry['client-id'] = this.clientId;
+        this.merchantId && (qry['merchant-id'] = this.merchantId);
+        this.currency && this.currency !== "USD" && (qry.currency = this.currency); // default: USD
+        this.intent && this.intent !== "capture" && (qry.intent = this.intent); // default: "capture"
+        this.commit === false && (qry.commit = this.commit);
+        this.vault === false && (qry.vault = this.vault); // default: false
+        this.disableFunding && (qry['disable-funding'] = this.disableFunding);
+        this.disableCard && (qry['disable-card'] = this.disableCard);
+        this.integrationDate && (qry['integration-date'] = this.integrationDate);
+        //this.debug && (qry.debug = this.debug);
+        this.buyerCountry && (qry['buyer-country'] = this.buyerCountry);
+        this.locale && (qry.locale = this.locale);
+        const query = Object.keys(qry).map(prop => `${prop}=${qry[prop]}`).join('&');
+        const href = `${endpoint}?${query}`;
+        this.debug && console.log(href);
+        return href
+      },
+      updatePayPalItems() {
+        if (!this.sdkLoaded || !this.orderInfo) {
+          console.log('sdk is not loaded or order info is missing');
+          return
+        }
+        
+        const _this = this;
+        let container = document.getElementById(_this.containerId);
+        if (!container) {
+          console.log('PayPal container missing')
+        } else {
+          container.innerText = '';
+          // add new PayPal button
+          paypal.Buttons({
+            locale: 'en_US',
+            style: {
+              size: 'responsive',
+              color:  'gold',
+              shape:  'pill',
+              label:  'pay',
+              height: 40
+            },
+            createOrder: async function (data, actions) {
+              if (this.useCustomCreateOrder) {
+                _this.$emit('onCreateOrder', data)
+              } else {
+                return actions.order.create(_this.orderInfo);
+              }
+            },
+            onApprove: async function (data, actions) {
+              if (this.useCustomCaptureOrder) {
+                _this.$emit('onApprove', data)
+              } else {
+                return actions.order.capture().then(function(details) {
+                  _this.$emit('onCaptured', details)
+                });
+              }
+            }
+          }).render(`#${this.containerId}`);
+        }
+      }
+    }
+  }
+</script>
